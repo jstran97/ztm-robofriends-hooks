4355efdaa9a8faf34dc08fc64a02efbc953b1bdf
Justin Tran <justin.s.tran97@gmail.com>
2025-03-03 13:04:00 -0800

    Converted code from class components to function components to incorporate hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+function App() {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchField: ''
+    //     }
+    // }
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
+    const [clickCount, setClickCount] = useState(0); // For useEffect() code experiment + useEffect() usage understanding
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => this.setState({ robots: users }));
+    // }
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
-    }
+            .then(users => {setRobots(users)});
 
-    onSearchChange = (event) => {
+        console.log(robots, searchField, clickCount);
+    }, []) // useEffect() runs only when a specifically listed state variable's state changes (i.e, if [clickCount] and clickCount num gets incremented)
+
+
+    const onSearchChange = (event) => {
         console.log(event.target.value);
         
-        this.setState({ searchField: event.target.value });
+        // this.setState({ searchField: event.target.value });
+        setSearchField(event.target.value);
     }
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        })
 
-        // console.log("filteredRobots: ", filteredRobots);
+    // const { robots, searchField } = this.state;
 
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        }
-        else {
-            return (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-        
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filteredRobots}/>
-                    </Scroll>
-                </div>
-            );
-        }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
 
+
+
+    // console.log("filteredRobots: ", filteredRobots);
+
+    if (robots.length === 0) {
+        return <h1>Loading...</h1>
     }
+    else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+    
+                {/*  // For useEffect() code experiment + useEffect() usage understanding */}
+                <button onClick={() => setClickCount(clickCount+1)}>Click Me!</button>
+
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }
+
+    
 }
 
 export default App;
