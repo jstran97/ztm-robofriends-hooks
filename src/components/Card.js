962ddd8c4f0de269f2d3b38e86489c82f37b60bd
Justin Tran <justin.s.tran97@gmail.com>
2025-10-03 17:51:10 -0700

    Adjusted Card component sizing to correspond with viewport size.

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,10 @@ const Card = ( { name, email, id } ) => {
         // -l (large): > 60em (~960px+)
         // -ns (not small): > 30em (~480px+)
 
-        // w-100 w-50-m w-third-l - Cards take full width on mobile, 50% on medium screens, 33% on large screens
+        // w-100 w-50-m w-third-l - Cards take full width on mobile, 50% on medium screens, 25% on large screens
         // f4 f3-m f2-l - Font sizes scale up on larger screens (f4=smaller, f2=larger)
         // w-100 w-auto-ns on image - Full width on mobile, auto on larger screens
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5 w-100 w-50-m w-third-l f4 f3-m f2-l w-auto-ns maxWidth'>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5 w-100 w-third-m w-fourth-l f4 f3-m f2-l w-auto-ns maxWidth'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
